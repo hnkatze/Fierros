940905bbdecb24ecffd9b7a43eedd5700df23ab3
katze <henriquezhector1@hotmail.com>
2024-06-17 11:44:17 -0600

    Modified, add a new function and type for a new form

--- src/config/Util.ts
+++ src/config/Util.ts
@@ -1,38 +1,52 @@
-import Resizer from 'react-image-file-resizer';
-import { Tags } from './type';
+import Resizer from "react-image-file-resizer";
+import { Tags } from "./type";
 
-export const compressImage = (file: File, maxWidth: number = 400, maxHeight: number = 400, quality: number = 40): Promise<File> => {
+export const compressImage = (
+  file: File,
+  maxWidth: number = 400,
+  maxHeight: number = 400,
+  quality: number = 40
+): Promise<File> => {
   return new Promise((resolve, reject) => {
     Resizer.imageFileResizer(
       file,
-      maxWidth, 
-      maxHeight, 
-      'JPEG', 
-      quality, 
-      0, 
+      maxWidth,
+      maxHeight,
+      "JPEG",
+      quality,
+      0,
       (uri) => {
-        if (typeof uri === 'string') {
-          const byteString = atob(uri.split(',')[1]);
+        if (typeof uri === "string") {
+          const byteString = atob(uri.split(",")[1]);
           const ab = new ArrayBuffer(byteString.length);
           const ia = new Uint8Array(ab);
           for (let i = 0; i < byteString.length; i++) {
             ia[i] = byteString.charCodeAt(i);
           }
-          const blob = new Blob([ab], { type: 'image/jpeg' });
-          const compressedFile = new File([blob], file.name, { type: 'image/jpeg' });
+          const blob = new Blob([ab], { type: "image/jpeg" });
+          const compressedFile = new File([blob], file.name, {
+            type: "image/jpeg",
+          });
           resolve(compressedFile);
         } else {
-          reject(new Error('Compression failed'));
+          reject(new Error("Compression failed"));
         }
       },
-      'base64'
+      "base64"
     );
   });
 };
 
 export function parseTags(tagsString: string): Tags[] {
-  return tagsString.split(',').map(tag => ({ tag: tag.trim() }));
+  return tagsString.split(",").map((tag) => ({ tag: tag.trim() }));
 }
 export function convertStringToTagsArray(tagsString: string): string[] {
-  return tagsString.split(',').map(tag => tag.trim());
+  return tagsString.split(",").map((tag) => tag.trim());
+}
+export function delayedReload(): Promise<void> {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
 }
