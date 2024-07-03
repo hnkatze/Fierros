c1001b9dec570ae755a8237327f9a7937948651c
HnKatze <henriquezhector1@homtail.com>
2024-07-03 09:15:29 -0600

    add a missing card in the dashboard

--- src/app/dash/page.tsx
+++ src/app/dash/page.tsx
@@ -1,20 +1,35 @@
 import Cards from "@/components/Cards";
 
-
-
-
-
 export default function Dash() {
-
-    return (
-        <main className="w-full h-6/7">
-            <div className="flex flex-wrap gap-5 justify-center items-center pt-12 h-6/7">
-            <Cards title="Nuevo" urlLink="/new" description="Aqui es donde haremos un nuevo ingreso." />
-            <Cards title="Buscar" urlLink="/find" description="Busquemos por DNI al contrubuyente" />
-            <Cards title="Galeria" urlLink="/gallery" description="Aqui podremos visualizar las imagenes." />
-            <Cards title="Contacto" urlLink="/contac" description="Vamos, si tienes algun problema, contactanos." />
-            </div>
-            </main>
-        
-    );
+  return (
+    <main className="w-full h-6/7">
+      <div className="flex flex-wrap gap-5 justify-center items-center pt-12 h-6/7">
+        <Cards
+          title="Nuevo"
+          urlLink="/new"
+          description="Aqui es donde haremos un nuevo ingreso."
+        />
+        <Cards
+          title="Buscar"
+          urlLink="/find"
+          description="Busquemos por DNI al contrubuyente"
+        />
+        <Cards
+          title="Galeria"
+          urlLink="/gallery"
+          description="Aqui podremos visualizar las imagenes."
+        />
+        <Cards
+          title="Contribuyentes"
+          urlLink="/contribu"
+          description="Revisemos las personas aqui."
+        />
+        <Cards
+          title="Contacto"
+          urlLink="/contac"
+          description="Vamos, si tienes algun problema, contactanos."
+        />
+      </div>
+    </main>
+  );
 }
